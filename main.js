72edbd317665d47f59d61ca4f269d02a2f132085
EthanMorrell <43864281+EthanMorrell@users.noreply.github.com>
2020-08-23 00:16:22 -1000

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-function highlightCurrentPage() {
-  document.getElementById("current").id = "";
+function removePageHighlight() {
+  document.getElementById("active").id = "inactive";
 }
+
+function replacePageHighlight() {
+  document.getElementById("inactive).id = "active";
+}
+
+document.getElementById("header").onmouseenter = "removePageHighlight()";
+document.getElementById("header").onmouseleave = "replacePageHighlight()";
