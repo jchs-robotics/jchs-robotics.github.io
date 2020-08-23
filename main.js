d1464b8ba0a89c6aa29297718ad74ab15130a326
EthanMorrell <43864281+EthanMorrell@users.noreply.github.com>
2020-08-23 00:25:52 -1000

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,3 @@ function removePageHighlight() {
 function replacePageHighlight() {
   document.getElementById("inactive").id = "active";
 }
-
-document.getElementById("header").onmouseenter = removePageHighlight();
-document.getElementById("header").onmouseleave = replacePageHighlight();
